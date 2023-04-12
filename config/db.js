61f3b235a845327fb6ea28c3c6ce2ff2c6247d78
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-04-12 19:12:10 +0530

    connection-url

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const connection = mongoose
-  .createConnection("mongodb://127.0.0.1:27017/contact")
+  .createConnection(
+    "mongodb+srv://jaspreetsodhi02:root@cluster0.2nkhc6f.mongodb.net/test"
+  )
+  // .createConnection("mongodb://127.0.0.1:27017/contact")
   .on("open", () => {
     console.log("Connected to MongoDb");
   })
